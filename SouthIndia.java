09c67115eb378d78863a1ffc4445bc848b0a6e28
Murugesan2520 <112553519+Murugesan2520@users.noreply.github.com>
2023-03-08 19:31:19 +0530

    OOPs-scenario4

---
+++ SouthIndia.java
@@ -0,0 +1,18 @@
+package Oops;
+
+public abstract class SouthIndia extends India  {
+
+	
+	void cultivate()
+	{
+		System.out.println("Rice and Wheat Cultivation");
+	}
+	void  livingStyle()
+	{
+		System.out.println("Average Development");
+	}
+	void nativeLanguage()
+	{
+		System.out.println("Tamil oldest language");
+	}
+}
